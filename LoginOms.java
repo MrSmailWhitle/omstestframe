29d2f6a1416eb2b0442fcd87b8593d953b643cf3
MrSmailWhitle <504727360@qq.com>
2017-03-08 18:43:47 +0800

    switch the baranch

--- LoginOms.java
+++ LoginOms.java
@@ -79,53 +79,68 @@ public class LoginOms{
 		System.out.println("[+......]readey add 客户合同");
 		
 	}
-	public void editCustomContract(){
+	//start：开始进行业务合同的添加编辑；
+	public void editCustomContractStart(){
 		wait.until(ExpectedConditions.presenceOfElementLocated(By.className("div_dialog_close")));
 		//browser.switchTo().frame("frm_Content_01");
 		WebElement iframe=browser.findElement(By.tagName("iframe"));
 		//browser.switchTo();
 		browser.switchTo().frame(iframe);
-
-		//是否二次开发
-		//if(browser.findElement(By.xpath("//*[@id='Form1']/table[1]/tbody/tr[1]/td[2]/input[2]")).isSelected()){
-			//browser.findElement(By.xpath("//*[@id='Form1']/table[1]/tbody[1]/tr[1]/td[2]/label[1]")).click();
-		//}
+	}
+	
+	//1。是否二次开发
+	//当前默认非二次开发；若是，label1.否：label2；
+	public void com_SencondDev(){
+		
 		browser.findElement(By.xpath("//*[@id='Form1']/table[1]/tbody[1]/tr[1]/td[2]/label[1]")).click();
 		//browser.findElement(By.xpath("//*[@id='Form1']/table[1]/tbody[1]/tr[1]/td[2]/label[1]")).click();
-		
+	}
+	//2.合同签订方式：包含三类
+	//a.新合同；b。补充协议；c.备忘录
+	public void switch_ContractSign(){
 		//选择合同签订方式
 		Select selectContractLawType=new Select(browser.findElement(By.id("ContractLawType")));
 		selectContractLawType.selectByVisibleText("新合同");
 		this.print("选择签订方式成功");
-		//选择合同适用范围
+		
+	}
+		//a-1.签订为新合同适用次方法
+		//新合同-合同范围
+	public void switch_ContractLimits(){
 		Select ContractLimits=new Select(browser.findElement(By.id("ContractLimits")));
 		ContractLimits.selectByVisibleText("中外籍");
 		this.print("范围成功");
-		
-		//选择合同类型
+	}
+		//a-1.选择新合同可以选择Fs1,FS2....;
+	    //2.；非新合同则只读
+	public void switch_ContractType(){
 		Select ContractType=new Select(browser.findElement(By.id("ContractType")));
 		ContractType.selectByVisibleText("FS1");
 		this.print("类型成功");
-		//生成业务合同编号
+	}
+		//a-3.生成业务合同编号
+		//新合同的时候可生成；非新合同合同类型只能选择；
 		//String js="funciton(){$('#Add_ContractNo').click();}function();";
 		//this.jsE(js);
+	public void switch_Add_ContractNo_orNot(){
 		browser.findElement(By.id("Add_ContractNo")).click();
 		
 		this.contractid=browser.findElement(By.id("txt_ContractNo")).getAttribute("value");
 		this.print("生成编号成功，切为："+this.contractid);
+	}
 		
-		
-		//选择签约方
+		//3.选择签约方
+	public void com_ContractParty(){
 		Select qy=new Select(browser.findElement(By.name("ContractParty")));
 		qy.selectByValue("2");
 		this.print("签约成功");
 		
-		//选择紧急程度
+		//4.选择紧急程度
 		Select hurrylevel=new Select(browser.findElement(By.id("ddl_notify_nevel")));
 		hurrylevel.selectByVisibleText("紧急");
 		this.print("紧急程度选择成功");
 	}
-	public void selectVerContract(String ver){
+	public void switch_selectVerContract(String ver){
 		if(ver.equals("标准")){
 			browser.findElement(By.id("ContractVerType1")).click();
 		}else if(ver.equals("非标准")){
@@ -155,7 +170,10 @@ public class LoginOms{
 		btmsave.click();
 		
 		
-		this.print("保存成功，请确定");
+		this.print("保存后请依据提示操作");
+	}	
+		
+	public void noWhole(){
 		Alert massange=browser.switchTo().alert();
 		assert massange.getText().contains("请填写所有数据"):"提示有误,请修改备注提示信息";
 		this.print("提示信息ok；");
